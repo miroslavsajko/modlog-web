fdb3747df6e0449450b7121ee281259c7310833d
Miroslav Sajko <miroslav77sajko@gmail.com>
2024-11-30 22:53:51 +0100

    Added axios to fetch posts

--- src/App.tsx
+++ src/App.tsx
@@ -2,40 +2,45 @@ import {useEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from "axios";
+
 const API_URL = import.meta.env.VITE_API_URL
+
+const fetchPosts = async () => {
+    console.info(API_URL)
+    return await axios.get(`${API_URL}/posts`).then(response => response.data).catch(reason => console.error(reason));
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
     useEffect(() => {
-        console.info(API_URL)
-        fetch(`${API_URL}/posts`).then(response => {
-            console.info(response.json())
-        });
+        fetchPosts().then(data => console.info(data));
     }, []);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo"/>
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo"/>
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    )
 }
 
 export default App
