dc7dfd2252116a994abd170c8dfb16d3d182d719
Ap4che <apache@atlas.sk>
2024-12-01 12:44:37 +0100

    Basic view without pagination and mod entries

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,111 @@
 import {useEffect, useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import axios from "axios";
+import "./css/global.css";
+import "react-data-grid/lib/styles.css";
+import DataGrid from "react-data-grid";
 
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL;
+
+const postsColumns = [
+	{ key: "title", name: "Title" },
+	{ key: "author", name: "Author" },
+	{ key: "flair", name: "Flair" },
+	{ key: "comments", name: "Comments" },
+	{ key: "upvotes", name: "Upvotes" }
+];
+
+// const modEntriesColumns = [
+// 	{ key: "action", name: "Action" },
+// 	{ key: "mod", name: "Moderator" },
+// 	{ key: "details", name: "Details" },
+// 	{ key: "description", name: "Description" },
+// 	{ key: "timestamp", name: "Timestamp" }
+// ];
 
 const fetchPosts = async () => {
-    console.info(API_URL)
-    return await axios.get(`${API_URL}/posts`).then(response => response.data).catch(reason => console.error(reason));
+	console.info(API_URL)
+	return await axios.get(`${API_URL}/posts`).then(response => response.data).catch(reason => console.error(reason));
+}
+
+// const fetchModEntries = async (modEntriesURL: string) => {
+// 	console.info(modEntriesURL);
+// 	return await axios.get(`${modEntriesURL}`).then(response => response.data).catch(reason => console.error(reason));
+// }
+
+interface Post {
+	title: string;
+	author: string;
+	flair: string;
+	score: number;
+	comments: number;
+	_links: {
+		self: string;
+		post: string;
+		modEntries: string;
+	};
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	modEntries: any[];
+}
+
+interface Page {
+	size: number;
+	totalElements: number;
+	totalPages: number;
+	number: number;
+}
+
+interface PostsAPI {
+	_embedded: {
+		posts: Post[];
+	};
+	_links: {
+		first: {
+			href: string;
+		};
+		self: {
+			href: string;
+		};
+		next: {
+			href: string;
+		};
+		last: {
+			href: string;
+		};
+		profile: {
+			href: string;
+		};
+	};
+	page: Page;
 }
 
 function App() {
-    const [count, setCount] = useState(0)
-    useEffect(() => {
-        fetchPosts().then(data => console.info(data));
-    }, []);
+	const [data, setData] = useState<PostsAPI | null>(null);
 
-    return (
-        <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo"/>
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo"/>
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
-    )
+	useEffect(() => {
+		const fetchData = async () => {
+			const data: PostsAPI = await fetchPosts();
+			console.log(data);
+			setData(data);
+		}
+
+		fetchData();
+	}, []);
+
+	return (
+		<>
+			<header>
+				<h1>r/Slovakia ModLog</h1>
+			</header>
+
+			<main>
+				<div className='grid-wrapper'>
+					<DataGrid columns={postsColumns} rows={data!._embedded.posts}/>
+				</div>
+			</main>
+
+			<footer></footer>
+		</>
+	)
 }
 
-export default App
+export default App;
