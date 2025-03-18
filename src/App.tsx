ff502927f0cd866135f90049826930422b8a3508
Martin Bati <martin.bati@student.tuke.sk>
2025-03-18 12:08:31 +0100

    cleaned fetch to api.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 // libs
 import { useEffect, useState } from 'react'
 // components
-import { API_URL, fetchModEntries, fetchPosts } from './components/api';
-import { ModEntriesAPI, Post, PostsAPI } from './components/interfaces';
+import { API_URL, fetchApiData } from './components/api';
+import { Post, PostsAPI } from './components/interfaces';
 import { Paginator } from './components/Paginator';
-import { convertDateTime } from './components/dateTimeConverter';
+import { DetailGrid, LoadingIcon, MainDataGrid, NoPostSelected } from './components/DataGrids';
 import Header from './components/Header';
 import Footer from './components/Footer';
 // css
 import "./css/global.scss";
 import "react-data-grid/lib/styles.css";
-import { DetailGrid, LoadingIcon, MainDataGrid, NoPostSelected } from './components/DataGrids';
 
 function App() {
 	const pageSize = 15;
@@ -20,30 +19,13 @@ function App() {
 	const [detailContent, setDetailContent] = useState<Post | null>(null);
 
 	useEffect(() => {
-		const fetchData = async () => {
-			const data: PostsAPI = await fetchPosts(pageURL);
-
-			for (const post of data._embedded.posts) {
-				post.timestamp = convertDateTime(post.timestamp);
-
-				const modEntriesData: ModEntriesAPI = await fetchModEntries(post._links.modEntries.href);
-				post.modEntries = modEntriesData;
-
-				post.modEntries._embedded.modentries.forEach(modEntry => {
-					modEntry.timestamp = convertDateTime(modEntry.timestamp);
-				})
-			}
-
-			setData(data);
-		}
-
-		fetchData();
+		fetchApiData({ pageURL, setData });
 	}, [pageURL]);
 
 	return (
 		<>
 			<Header />
-
+		
 			<main>
 				{data === null ? <LoadingIcon /> : <MainDataGrid data={data} setDetailContent={setDetailContent} />}
 
--- src/components/api.ts
+++ src/components/api.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { ModEntriesAPI, PostsAPI } from "./interfaces";
+import { convertDateTime } from "./dateTimeConverter";
 export const API_URL: string = import.meta.env.VITE_API_URL;
 const DEBUG_MODE = 0;
 
@@ -31,4 +33,26 @@ export const fetchModEntries = async (modEntriesURL: string) => {
 	// urls in APIs are returned with http, not https -> throwing CORS error
 	const moddedURL = modEntriesURL.replace(/^http:\/\//, 'https://');
 	return await axios.get(moddedURL).then(response => response.data).catch(reason => console.error(reason));
+}
+
+interface FetchInterface {
+	pageURL: string;
+	setData: React.Dispatch<React.SetStateAction<PostsAPI | null>>;
+}
+
+export async function fetchApiData({ pageURL, setData }: FetchInterface) {
+	const data: PostsAPI = await fetchPosts(pageURL);
+
+	for (const post of data._embedded.posts) {
+		post.timestamp = convertDateTime(post.timestamp);
+
+		const modEntriesData: ModEntriesAPI = await fetchModEntries(post._links.modEntries.href);
+		post.modEntries = modEntriesData;
+
+		post.modEntries._embedded.modentries.forEach(modEntry => {
+			modEntry.timestamp = convertDateTime(modEntry.timestamp);
+		});
+	}
+
+	setData(data);
 }
