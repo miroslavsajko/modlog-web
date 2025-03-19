80ede3a7f8c3624eaba7b54eac4f9516bdddaeb5
Martin Bati <martin.bati@student.tuke.sk>
2025-03-19 19:04:12 +0100

    fixed overflowing long titles/authors

--- src/components/api.ts
+++ src/components/api.ts
@@ -3,6 +3,8 @@ import { ModEntriesAPI, PostsAPI } from "./interfaces";
 import { convertDateTime } from "./dateTimeConverter";
 export const API_URL: string = import.meta.env.VITE_API_URL;
 const DEBUG_MODE = 0;
+const MAX_TITLE_LENGTH = 128;
+const MAX_USERNAME_LENGTH = 24;
 
 export const postsColumns = [
 	{ key: "title", name: "Title" },
@@ -30,7 +32,7 @@ export const fetchPosts = async (requestedURL: string) => {
 export const fetchModEntries = async (modEntriesURL: string) => {
 	if(DEBUG_MODE)
 		console.info(modEntriesURL);
-	// urls in APIs are returned with http, not https -> throwing CORS error
+	
 	const moddedURL = modEntriesURL.replace(/^http:\/\//, 'https://');
 	return await axios.get(moddedURL).then(response => response.data).catch(reason => console.error(reason));
 }
@@ -40,11 +42,17 @@ interface FetchInterface {
 	setData: React.Dispatch<React.SetStateAction<PostsAPI | null>>;
 }
 
+function truncateString(str: string, maxLength: number): string {
+	return str.length > maxLength ? str.substring(0, maxLength) + "..." : str;
+}
+
 export async function fetchApiData({ pageURL, setData }: FetchInterface) {
 	const data: PostsAPI = await fetchPosts(pageURL);
 
 	for (const post of data._embedded.posts) {
 		post.timestamp = convertDateTime(post.timestamp);
+		post.title = truncateString(post.title, MAX_TITLE_LENGTH);
+		post.author = truncateString(post.author, MAX_USERNAME_LENGTH);
 
 		const modEntriesData: ModEntriesAPI = await fetchModEntries(post._links.modEntries.href);
 		post.modEntries = modEntriesData;
