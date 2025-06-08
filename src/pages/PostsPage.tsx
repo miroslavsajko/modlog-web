c8ac1e6798d14c418957849dafff04fac0a50c4b
Miroslav Sajko <miroslav77sajko@gmail.com>
2025-06-08 22:08:13 +0200

    added cursor on details icon

--- src/pages/PostsPage.tsx
+++ src/pages/PostsPage.tsx
@@ -243,7 +243,8 @@ export default function PostsPage() {
                                 sx={{
                                     display: 'flex',
                                     alignItems: 'center',
-                                    color: 'inherit'
+                                    color: 'inherit',
+                                    cursor: 'pointer'
                                 }}
                                 onClick={() => {
                                     setselectedPostId(data.postid)
